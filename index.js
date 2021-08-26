cae829ee735e22ff1e430e205a53ec8f07400ae2
leaharro <leaha@live.com>
2021-08-25 21:39:45 -0500

    Homework19
    Hope this works. Take 3

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 function fade() {
     $('.preloader').fadeOut("slow");
     }
-    setTimeout(fade, 3000);
+    setTimeout(fade, 1000);
+
+    $(window).load(function() { 
+        $("#status").fadeOut("slow"); 
+        $("#loader").delay(500).fadeOut();
+        });
+
+    jQuery(document).ready(function($) {
+        var Body = $('body');
+        Body.addClass('preloader-site');
+      });
+      $(window).load(function(){
+      $('.preloader-wrapper').fadeOut();
+        $('body').removeClass('preloader-site');
+      });
