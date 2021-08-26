79242bb62c348d3f57888585610c59882dbdc39e
leaharro <leaha@live.com>
2021-08-26 13:00:08 -0500

    Hmwk 19
    Hope this works take 3

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-function fade() {
-    $('.preloader').fadeOut("slow");
-    }
-    setTimeout(fade, 1000);
-
-    $(window).load(function() { 
-        $("#status").fadeOut("slow"); 
-        $("#loader").delay(500).fadeOut();
-        });
-
-    jQuery(document).ready(function($) {
-        var Body = $('body');
-        Body.addClass('preloader-site');
-      });
-      $(window).load(function(){
-      $('.preloader-wrapper').fadeOut();
-        $('body').removeClass('preloader-site');
-      });
+$(window).load(function() {
+	$(".loader").delay(2000).fadeOut("slow");
+  $(".overlayer").delay(2000).fadeOut("slow");
+})
